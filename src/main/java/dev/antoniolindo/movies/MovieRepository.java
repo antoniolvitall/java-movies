b48aac84a0f22d9500958ac6f5be96324204e0ab
Antonio Lindo <antoniolvitall@gmail.com>
2023-07-13 19:37:31 -0300

    reiniciando o projeto

--- src/main/java/dev/antoniolindo/movies/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.antoniolindo.movies;
-
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
-}
